0132163ff5138ba7e1e833145bb2e556daa5eef3
NamNV609 <namnv609@gmail.com>
2018-05-12 16:50:45 +0700

    Add admin layout

---
+++ resources/lang/vi/admins.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Admin language
+ */
+
+return [
+    "layout" => [
+        "top_nav" => [
+            "my_profile" => "Tài khoản",
+            "logout" => "Thoát"
+        ],
+        "sidebar" => [
+            "dashboard" => "Bảng điều khiển",
+            "album" => "Albums",
+            "list" => "Danh sách",
+            "new" => "Thêm mới",
+            "song" => "Bài hát",
+            "configuration" => "Cấu hình"
+        ],
+    ],
+];
