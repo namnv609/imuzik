06264380869e513a99939a11ceac2215085c9640
NamNV609 <namnv609@gmail.com>
2018-05-12 17:26:35 +0700

    Add migrations and users table seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            "name" => "Administrator",
+            "email" => "admin@local.tld",
+            "password" => Hash::make("123456"),
+        ]);
+    }
+}
